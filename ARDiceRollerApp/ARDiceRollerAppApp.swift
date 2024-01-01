b0fdb852d33f7fe4ce3f00a0afbd0794b8ea044d
Ishaan Goel <goelishaan2007@gmail.com>
2024-01-01 15:07:45 -0800

    Initial Commit

---
+++ ARDiceRollerApp/ARDiceRollerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARDiceRollerAppApp.swift
+//  ARDiceRollerApp
+//
+//  Created by Ishaan Goel on 1/1/24.
+//
+
+import SwiftUI
+
+@main
+struct ARDiceRollerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
